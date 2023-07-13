f5dab19dfc33b9ef6b174e6c78337e76dbaea3c7
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2023-07-13 23:25:49 +0530

    Kadane Algorithm

---
+++ MaxiSubArray.java
@@ -0,0 +1,16 @@
+class solution{
+    public int maxSubArray(int nums[]){
+        int sum=0;
+        int max=nums[0];
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+            if(sum>max){
+                max=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return max;
+    }
+}
