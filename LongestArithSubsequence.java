8f3fbbe4b6ddefa5caf36155926de1c0b0849808
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2023-07-14 22:25:54 +0530

    longest arithmetic subsequence

---
+++ LongestArithSubsequence.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int longestArithSeqLength(int[] nums) {
+     int n = nums.length;
+        if (n <= 2)
+            return n;
+
+        int longestseq= 2;
+        Map<Integer, Integer>[] dp = new HashMap[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = new HashMap<>();
+            for (int j = 0; j < i; j++) {
+                int diff = nums[i] - nums[j];
+                dp[i].put(diff, dp[j].getOrDefault(diff, 1) + 1);
+                longestseq= Math.max(longestseq, dp[i].get(diff));
+            }
+        }
+
+        return longestseq;
+    }
+}
