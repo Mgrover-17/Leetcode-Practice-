af10e98875fc96827e5ca74304e826fd565d41d2
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2023-07-16 23:44:01 +0530

    queue reverse

---
+++ queueRev.java
@@ -0,0 +1,19 @@
+import java.util.Queue;
+import java.util.Stack;
+
+public class queueRev {
+    public Queue<Integer>rev(Queue<Integer>q){
+        Stack<Integer>newStack=new Stack<>();
+        while(!q.isEmpty()){
+            int ele=q.poll();
+            newStack.push(ele);
+        }
+        while(!newStack.isEmpty()){
+            q.offer(newStack.pop());
+        }
+        return q;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
