91a347591ef92ddabc922cecff35ad50e247f7b9
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2023-07-17 23:51:50 +0530

    First non-repeating character in a stream

---
+++ FirstNonRepeatingCharacter.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+class Solution
+{
+    public String FirstNonRepeating(String A)
+    {
+       StringBuilder ans = new StringBuilder();
+
+        int arr[] = new int[26];
+        ArrayList<Character> list = new ArrayList<>();
+        for (int i = 0; i < A.length(); i++) {
+            char ch = A.charAt(i);
+            if (arr[ch - 'a'] == 0) {
+                list.add(ch);
+            }
+
+            arr[ch - 'a']++;
+
+            int flag = 0;
+            int m = list.size();
+
+            for (int j = 0; j < m; j++) {
+                char chs = list.get(j);
+
+                if (arr[chs - 'a'] == 1) {
+                    ans.append(chs);
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                ans.append("#");
+            }
+
+        }
+        return ans.toString();
+    }
+}
