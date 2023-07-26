caad661577298d024ac31ed6c2254b33e70c62f4
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2023-07-26 21:54:48 +0530

    ..

---
+++ KthAncestorTree.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Node
+{
+    int ans ;
+    private int data;
+    private Node right;
+    private Node left;
+    public int kthAncestor(Node root, int k, int node)
+    {
+        //Write your code here
+        ArrayList<Integer> al = new ArrayList<>();
+        helper(root,k,node,al);
+        return ans;
+    }
+    
+    void helper(Node root,int k, int node, ArrayList<Integer> al) {
+        if(root== null) {
+            return;
+        }
+        if(root.data == node) {
+            if(al.size() < k)    ans =-1;
+            else    ans = al.get(al.size()-k);
+            return;
+        } 
+        al.add(root.data);
+        helper(root.left,k,node,al);
+        helper(root.right,k,node,al);
+        al.remove(al.size()-1);
+    }
+}
