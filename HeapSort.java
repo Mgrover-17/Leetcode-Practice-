a66b6783cd3c886eed8949ecae8556506ac9947a
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2023-07-27 22:34:16 +0530

    ..

---
+++ HeapSort.java
@@ -0,0 +1,46 @@
+class Solution
+{
+    //Function to build a Heap from array.
+    void buildHeap(int arr[], int n)
+    {
+        // Your code here
+        for(int i=n-2/2;i>=0;i--)
+        {
+            heapify(arr,n,i);
+        }
+    }
+ 
+    //Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i)
+    {
+        // Your code here
+        int largest=i;
+        int left=2*i+1;
+        int right=2*i+2;
+        if(left<n && arr[left]>arr[largest])
+           largest=left;
+        if(right<n && arr[right]>arr[largest])
+           largest=right; 
+        if(largest!=i)
+        {
+            int temp=arr[largest];
+            arr[largest]=arr[i];
+            arr[i]=temp;
+            heapify(arr,n,largest);
+        }
+    }
+    
+    //Function to sort an array using Heap Sort.
+    public void heapSort(int arr[], int n)
+    {
+        //code here
+        buildHeap(arr,n);
+        for(int i=n-1;i>=1;i--)
+        {
+            int temp=arr[0];
+            arr[0]=arr[i];
+            arr[i]=temp;
+            heapify(arr,i,0);
+        }
+    }
+ }
