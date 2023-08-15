8249e8a8316ef86b580a549aa0afb613b25c6250
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2023-08-15 23:29:10 +0530

    .

---
+++ PartitionList.java
@@ -0,0 +1,43 @@
+
+class PartitionList {
+    class ListNode{
+    private int val;
+    private PartitionList.ListNode next;
+    
+
+    public ListNode(int i) {
+        }
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode current = head;  // Pointer to traverse the original list
+        
+        ListNode lessDummy = new ListNode(0);    // Dummy node for nodes < x
+        ListNode lessTail = lessDummy;           // Tail pointer for less list
+        
+        ListNode greaterDummy = new ListNode(0); // Dummy node for nodes >= x
+        ListNode greaterTail = greaterDummy;     // Tail pointer for greater list
+        
+        // Traverse the original list
+        while (current != null) {
+            if (current.val < x) {
+                // Append current node to the less list
+                lessTail.next = new ListNode(current.val);
+                lessTail = lessTail.next; // Move the tail pointer
+            } else {
+                // Append current node to the greater list
+                greaterTail.next = new ListNode(current.val);
+                greaterTail = greaterTail.next; // Move the tail pointer
+            }
+            current = current.next; // Move to the next node
+        }
+        
+       
+        
+        // Attach the greater list to the end of the less list
+        lessTail.next = greaterDummy.next;
+        
+        // Return the modified list starting from the first node after the less dummy node
+        return lessDummy.next;
+    }
+    }
+}
